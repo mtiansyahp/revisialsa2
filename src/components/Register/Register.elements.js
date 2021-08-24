3cfe0dd48bae7abaff9517041b5bca3dcf837dfb
mtiansyahpratamaa <53297603+mtiansyahp@users.noreply.github.com>
2021-08-24 10:22:37 +0700

    update 2

--- src/components/Register/Register.elements.js
+++ src/components/Register/Register.elements.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const InfoSec = styled.div`
   color: #fff;
   padding: 160px 0;
-  background: ${({ lightBg }) => (lightBg ? '#fff' : '#000')};
+  background: ${({ lightBg }) => (lightBg ? '#fff' : '#ffff')};
 `;
 
 export const InfoRow = styled.div`
